0b0ca517faab96a02fc8d46d1f7a1581d114cd77
CrisGO0510 <cristhiangiraldo053@gmail.com>
2025-04-07 12:37:20 -0500

    Modulo de fuctional area finalizado

---
+++ custom_addons/functional_area/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Dependencia Funcional",
+    "version": "0.1",
+    "depends": ["base"],
+    "author": "NePinFe",
+    "data": [
+        "security/ir.model.access.csv",
+        "views/functional_area_form.xml",
+        "views/functional_area_list.xml",
+        "views/functional_area_views.xml",
+        "views/functional_area_menu.xml",
+    ],
+    "installable": True,
+    "application": True,
+    "license": "LGPL-3",
+}
