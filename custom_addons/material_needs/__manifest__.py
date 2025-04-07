7aab9c7139947181ec2d401498901c455a26ff9f
CrisGO0510 <cristhiangiraldo053@gmail.com>
2025-04-06 23:50:09 -0500

    Se movió equipment_needs a custom_addons

---
+++ custom_addons/material_needs/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "material_needs",
+    "version": "0.1",
+    "depends": ["base"],
+    "author": "Criko",
+    "description": "Long description of module's purpose",
+    "data": [
+        "views/views.xml",
+        "security/ir.model.access.csv",
+        # "views/templates.xml",
+    ],
+    "installable": True,
+    "application": True,
+}
---
+++ custom_addons/material_needs/models/models.py
@@ -0,0 +1,20 @@
+from odoo import models, fields, api
+
+
+class MaterialNeeds(models.Model):
+    _name = "material_needs.material_needs"
+    _description = "Material Requirements"
+
+    material_name = fields.Char(string="Material Name", required=True)
+    quantity = fields.Integer(string="Quantity", default=1)
+    unit_cost = fields.Float(string="Unit Cost")
+    # approval_status_id = fields.Many2one("approval.status", string="Approval status")
+    approval_status_id = fields.Integer(string="Approval Status")
+    total_cost = fields.Float(
+        string="Total Cost", compute="_compute_total_cost", store=True
+    )
+
+    @api.depends("quantity", "unit_cost")
+    def _compute_total_cost(self):
+        for record in self:
+            record.total_cost = record.quantity * record.unit_cost
