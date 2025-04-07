05ffff69957f29ecce092768f58792aea0e347c5
CrisGO0510 <cristhiangiraldo053@gmail.com>
2025-04-07 15:33:14 -0500

    Tipo de estado de approvación implementado

---
+++ custom_addons/approval_status/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Estado de Aprobación",
+    "version": "0.1",
+    "depends": ["base"],
+    "author": "NePinFe",
+    "data": [
+        "security/ir.model.access.csv",
+        "views/type_views/approval_status_type_form.xml",
+        "views/type_views/approval_status_type_list.xml",
+        "views/type_views/approval_status_type_views.xml",
+        "views/type_views/approval_status_type_menu.xml",
+    ],
+    "installable": True,
+    "application": True,
+    "license": "LGPL-3",
+}
---
+++ custom_addons/approval_status/models/models.py
@@ -0,0 +1,24 @@
+from odoo import models, fields
+
+
+class approval_status_type(models.Model):
+    _name = "approval_status_type.approval_status_type"
+    _description = "approval_status_.approval_status_type"
+
+    name = fields.Char(string="Status", required=True)
+    description = fields.Text(string="Description")
+
+
+class approval_status_log(models.Model):
+    _name = "approval_status_log.approval_status_log"
+    _description = "approval_status_log.approval_status_log"
+
+    module_id = fields.Integer(string="Module ID", required=True)
+
+    old_status_id = fields.Many2one("approval_status_type", string="Previous Status")
+    new_status_id = fields.Many2one("approval_status_type", string="New Status")
+
+    # user_id = fields.Many2one("res.users", string="Changed By", default=lambda self: self.env.user)
+    timestamp = fields.Datetime(string="Change Time", default=fields.Datetime.now)
+
+    user_id = fields.Integer(string="Changed By")
