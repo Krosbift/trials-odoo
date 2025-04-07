ba30e826006a11573249487bec6003366ba03673
CrisGO0510 <cristhiangiraldo053@gmail.com>
2025-04-07 18:08:36 -0500

    Solicitud de modulo finalizado

---
+++ custom_addons/module_requester/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Solicitud de Modulo",
+    "version": "0.1",
+    "depends": ["base"],
+    "author": "NePinFe",
+    "data": [
+        "security/ir.model.access.csv",
+        "data/modules_data.xml",
+        "views/module_requester_form.xml",
+        "views/module_requester_list.xml",
+        "views/module_requester_views.xml",
+        "views/module_requester_menu.xml",
+    ],
+    "installable": True,
+    "application": True,
+    "license": "LGPL-3",
+}
