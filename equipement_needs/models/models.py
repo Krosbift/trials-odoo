f502eb7b08350c50d3c2f42baa5adcd28dd389f3
Di3go0-0 <d.rincon@utp.edu.co>
2025-04-06 23:41:50 -0500

    update modela

--- equipement_needs/models/models.py
+++ equipement_needs/models/models.py
@@ -1,4 +1,5 @@
-from odoo import models, fields
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
 
 
 class equipement_needs(models.Model):
@@ -8,4 +9,16 @@ class equipement_needs(models.Model):
     equipement = fields.Char(string="Equipement", required=True)
     quantity = fields.Integer(string="Quantity", required=True)
     unit_cost = fields.Float(string="Unit Cost", required=True)
+
     # description = fields.Text()
+    @api.constrains("quantity")
+    def _check_positive_quantity(self):
+        for record in self:
+            if record.quantity <= 0:
+                raise ValidationError("Quantity must be a positive integer")
+
+    @api.constrains("unit_cost")
+    def _check_positive_unit_cost(self):
+        for record in self:
+            if record.unit_cost <= 0:
+                raise ValidationError("Unit Cost must be a positive float")
