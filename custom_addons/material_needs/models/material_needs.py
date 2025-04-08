d86eed11cb24118ca89fc8bd137a5be82d7222cc
CrisGO0510 <cristhiangiraldo053@gmail.com>
2025-04-07 21:39:57 -0500

    Auditoria implementado

--- custom_addons/material_needs/models/material_needs.py
+++ custom_addons/material_needs/models/material_needs.py
@@ -24,3 +24,19 @@ class material_needs(models.Model):
     def _compute_total_cost(self):
         for record in self:
             record.total_cost = record.quantity * record.unit_cost
+
+    def write(self, vals):
+        for record in self:
+            old_status = record.approval_status_id
+
+            res = super(material_needs, record).write(vals)
+
+            if "approval_status_id" in vals and vals["approval_status_id"] != (old_status.id if old_status else None):
+                self.env["approval_status_log.approval_status_log"].create([{
+                    "module_id": record.id,
+                    "old_status_id": old_status.id if old_status else False,
+                    "new_status_id": vals["approval_status_id"],
+                    "user_id": self.env.uid,
+                }])
+
+        return res
