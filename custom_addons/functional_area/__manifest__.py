3cb2bdc17cbac76a62fdff05b25c227f22e74af2
krosbift <torifaoficial@gmail.com>
2025-04-07 23:17:10 -0500

    merge with criko

--- custom_addons/functional_area/__manifest__.py
+++ custom_addons/functional_area/__manifest__.py
@@ -17,5 +17,6 @@
     ],
     "installable": True,
     "application": True,
+    'auto_install': True,
     "license": "LGPL-3",
 }
--- custom_addons/module_requester/__manifest__.py
+++ custom_addons/module_requester/__manifest__.py
@@ -5,11 +5,14 @@
     "author": "NePinFe",
     "data": [
         "security/ir.model.access.csv",
-        "data/modules_data.xml",
+        "data/module_requester_data.xml",
         "views/module_requester_form.xml",
         "views/module_requester_list.xml",
         "views/module_requester_views.xml",
         "views/module_requester_menu.xml",
+    ],
+    "demo": [
+        "demo/module_requester_demo.xml",
     ],
     "installable": True,
     "application": True,
